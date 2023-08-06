e293c465feb3100f0035b232b65d7be85309505c
Glenn Do <you@example.com>
2023-08-06 16:08:21 -0500

    fixing mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,10 +92,7 @@ const Home: NextPage = ({ prompts }: { prompts: Prompt[] }) => {
     <>
       <main className="mx-auto max-w-[1960px] p-4">
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
-            <div className="absolute inset-0 flex items-center justify-center opacity-20">
-              <span className="absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
-            </div>
+          <div className="after:content relative mb-5 flex h-auto flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
             <button
               className="pointer z-10 mt-3 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-2"
               onClick={handleClick}
@@ -146,10 +143,12 @@ const Home: NextPage = ({ prompts }: { prompts: Prompt[] }) => {
                 onKeyDown={handleKeyDown}
               />
             </form>
+
+            {guesses.map((guess: String) => (
+              <p className="z-50 font-semibold text-white">{guess}</p>
+            ))}
           </div>
-          {guesses.map((guess: String) => (
-            <p className="z-50 font-semibold text-white">{guess}</p>
-          ))}
+
           {state.map((result: Result) => (
             <Image
               alt="Next.js Conf photo"
