37b5fd45d97a0ec4d0d5dfa09ec4aa5d508e2e7c
Glenn Do <you@example.com>
2023-08-06 15:12:55 -0500

    change title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -6,21 +6,21 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
+          <meta name="description" content="Play a word guessing game" />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:description" content="Play a word guessing game" />
           <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            property="og:title"
+            content="Wordage, an AI word guessing game"
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta
+            name="twitter:title"
+            content="Wordage, an AI word guessing game"
+          />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Play a word guessing game"
           />
         </Head>
         <body className="bg-black antialiased">
@@ -28,8 +28,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
